1e29e584723f91efab1461d1f80286ade1ddc148
Matthias Nötzli <matthias.noetzli@gmail.com>
2023-10-18 18:10:24 +0200

    Create Search.vue

---
+++ resources/js/Components/Search.vue
@@ -0,0 +1,42 @@
+<template>
+    <form @submit.prevent="search" class="relative w-full max-w-[60ch]">
+        <div class="flex items-end gap-4">
+            <HfhInput
+                v-model="inputValue"
+                id="search"
+                type="search"
+                placeholder="Suchbegriff"
+                label="Volltextsuche"
+            />
+            <HfhButton type="submit" icon="search" :animated="false"
+                >Suchen</HfhButton
+            >
+        </div>
+    </form>
+</template>
+
+<script setup lang="ts">
+import { HfhButton, HfhInput } from "@hfh-dlc/hfh-styleguide";
+import { ref, watch } from "vue";
+const emit = defineEmits(["search"]);
+const props = defineProps({
+    searchText: {
+        type: String,
+        default: "",
+    },
+});
+const inputValue = ref(props.searchText);
+
+watch(
+    () => props.searchText,
+    (newVal) => {
+        inputValue.value = newVal;
+    }
+);
+
+const search = () => {
+    emit("search", inputValue);
+};
+</script>
+
+<style scoped></style>
