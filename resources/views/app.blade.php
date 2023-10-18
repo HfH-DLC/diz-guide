dcfba75b2ecd3ba03e6a2c87608c9aa68cf0314c
Matthias Nötzli <matthias.noetzli@gmail.com>
2023-10-18 18:10:14 +0200

    Add app.scss

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,8 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    @vite('resources/scss/app.scss')
     @vite('resources/js/app.js')
     @inertiaHead
   </head>
