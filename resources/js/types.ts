dfc4b78096db397ca1c22e7e8cd0f12ad30f8379
Matthias Nötzli <matthias.noetzli@gmail.com>
2023-10-18 18:10:21 +0200

    Create types.ts

---
+++ resources/js/types.ts
@@ -0,0 +1,9 @@
+export interface ResultData {
+    mainCategory: string;
+    mediaType: string;
+    signatureAndTopic: string;
+    location: {
+        label: string;
+        imageSrc: string;
+    };
+}
