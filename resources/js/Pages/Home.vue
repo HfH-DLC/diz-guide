4ae8258ffdd7627e69f6317e6f9090f39b3afb8e
Matthias Nötzli <matthias.noetzli@gmail.com>
2023-10-19 16:09:50 +0200

    Update Home.vue

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -12,27 +12,83 @@
             </div>
         </div>
     </header>
-    <main class="mx-4 xl:max-w-container xl:mx-auto">
+    <main class="mx-4 xl:max-w-container xl:mx-auto pb-20">
         <div class="flex items-center flex-col">
             <p>Erkunden Sie das Angebot des DiZ.</p>
             <div class="mt-16 w-full flex justify-center">
                 <Search></Search>
             </div>
+            <HfhFilterGroup class="mt-8 w-full"></HfhFilterGroup>
             <div class="mt-16 w-full">
                 <LoadingIndicator v-if="loading" class="mx-auto" />
                 <Results v-else :results="results"></Results>
             </div>
         </div>
     </main>
+    <footer>
+        <HfhFooter>
+            <template #tagline
+                ><p>
+                    wissenschaftsbasiert, praxisorientiert, breit verankert
+                </p></template
+            >
+            <template #contact-address
+                ><p>
+                    Schaffhauserstrasse 239<br />Postfach 5850<br />CH-8050
+                    Zürich
+                </p></template
+            >
+            <template #contact-other>
+                <p>T +41 44 317 11 11</p>
+                <p><a href="mailto:info@hfh.ch">info@hfh.ch</a></p>
+            </template>
+            <template #socials>
+                <HfhSocialBlock
+                    icon="facebook"
+                    href="https://www.facebook.com/hfh.edu"
+                />
+                <HfhSocialBlock
+                    icon="youtube"
+                    href="https://www.youtube.com/user/hfhzuerich"
+                />
+                <HfhSocialBlock
+                    icon="linkedin"
+                    href="https://www.linkedin.com/company/hfh.edu"
+                />
+                <HfhSocialBlock
+                    icon="instagram"
+                    href="https://www.instagram.com/hfh_edu/"
+                />
+                <HfhSocialBlock
+                    icon="twitter"
+                    href="https://twitter.com/hfh_edu"
+                />
+                <HfhSocialBlock
+                    icon="issuu"
+                    href="https://issuu.com/hochschule_fuer_heilpaedagogik"
+                />
+            </template>
+            <template #copyright
+                >© Copyright {{ new Date().getFullYear() }} HfH</template
+            >
+        </HfhFooter>
+    </footer>
 </template>
 
 <script setup lang="ts">
-import { HfhHeaderBar, HfhLogo, HfhMenu } from "@hfh-dlc/hfh-styleguide";
+import {
+    HfhFilterGroup,
+    HfhHeaderBar,
+    HfhLogo,
+    HfhMenu,
+    HfhFooter,
+} from "@hfh-dlc/hfh-styleguide";
 import Search from "../Components/Search.vue";
 import LoadingIndicator from "../Components/LoadingIndicator.vue";
 import Results from "../Components/Results.vue";
 import { ref } from "vue";
 import type { ResultData } from "../types";
+import { HfhSocialBlock } from "@hfh-dlc/hfh-styleguide";
 
 const menuItems = [
     { label: "hfh.ch", link: { href: "https://hfh.ch", target: "_blank" } },
@@ -46,7 +102,8 @@ const results: Array<ResultData> = [
         signatureAndTopic: "1.1.3.1 Grafomotorik",
         location: {
             label: "Rollgestell Nr.1",
-            imageSrc: "#",
+            imageSrc: "images/Rollgestelle_mit_Beschriftung.png",
+            imageAlt: "",
         },
     },
     {
@@ -55,7 +112,8 @@ const results: Array<ResultData> = [
         mediaType: "Buch",
         location: {
             label: "Rollgestell Nr.1",
-            imageSrc: "#",
+            imageSrc: "images/Rollgestelle_mit_Beschriftung.png",
+            imageAlt: "",
         },
     },
 ];
